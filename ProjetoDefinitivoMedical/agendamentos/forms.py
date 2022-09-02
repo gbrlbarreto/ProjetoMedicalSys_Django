771132e242db2c5fa00d8ecca788d2d396a3cc65
matheusnelvam <matheus.nelvam@gmail.com>
2022-09-01 22:01:14 -0300

    test

---
+++ ProjetoDefinitivoMedical/agendamentos/forms.py
@@ -0,0 +1,13 @@
+from dataclasses import field
+from django import forms
+from django.forms import ModelForm
+from .models import Agendamento
+
+#Classe do formulário de Person
+class AgendamentoForm(ModelForm):
+    class Meta:
+        model = Agendamento
+        fields = ['data', 'descricao', 'status', 'paciente']
+
+
+
