786f9ca43931c4f128eb6a43179cddcc7b526840
Gabriel Barreto <gbrlbarreto72@hotmail.com>
2022-09-07 11:02:37 -0300

    Adicao pgn cadastrado com sucesso e erro

--- medicos/urls.py
+++ medicos/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import cadastro
+from .views import cadastrado, cadastro, erro_cadastro
 
 #Listas de URLs para serem acessadas
 urlpatterns = [
     path('cadastro/', cadastro, name="cadastro"),
+    path('cadastrado/', cadastrado, name="cadastrado"),
+    path('erro/', erro_cadastro, name="erro_cadastro"),
+
 ]
