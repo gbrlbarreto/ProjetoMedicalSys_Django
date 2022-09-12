5db66dae2d6f50cab4d3b4107a79c976081e7718
Gabriel Barreto Tavares da Silva <gabriel.barreto@labs.dcx>
2022-09-12 14:46:48 -0300

    Issue #13 - Concluída

--- agendamentos/models.py
+++ agendamentos/models.py
@@ -15,6 +15,8 @@ class Agendamento(models.Model):
     status = models.CharField(max_length=12, choices=STATUS_CHOICES)
     medico = models.ForeignKey(User, on_delete=models.CASCADE)
     paciente = models.ForeignKey(Person, on_delete=models.CASCADE)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
     class Meta:
         db_table = "agendamentos"
