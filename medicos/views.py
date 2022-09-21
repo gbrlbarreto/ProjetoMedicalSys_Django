7961d146af1e4aa621d281ad9383de38b16bec91
Gabriel Barreto Tavares da Silva <gabriel.barreto@labs.dcx>
2022-09-21 10:59:34 -0300

    Alteracoes DateTime

--- medicos/views.py
+++ medicos/views.py
@@ -6,7 +6,6 @@ from .serializers import MedicoSerializer
 from rest_framework import viewsets
 
 
-
 class MedicoViewSet(viewsets.ModelViewSet):
     queryset = Medico.objects.all()
     serializer_class = MedicoSerializer
