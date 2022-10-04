1ccf4190b8d14edf649a0bdd2c2c56dac8cf17a2
matheusnelvam <matheus.nelvam@gmail.com>
2022-10-03 22:39:44 -0300

    models

--- agendamentos/models.py
+++ agendamentos/models.py
@@ -21,3 +21,17 @@ class Agendamento(models.Model):
     
     class Meta:
         db_table = "agendamentos"
+
+class Events(models.Model):
+    evento_id = models.AutoField(primary_key=True)
+    evento_name = models.CharField(max_length=255, null=True, blank=True)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    event_type = models.CharField(max_length=10, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.event_name)
+
+    class Meta:
+        verbose_name = 'Event'
+        verbose_name_plural = 'Events'        
