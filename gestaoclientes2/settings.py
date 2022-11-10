fde75b1a636d9a1865c8e58e0ee9a5a156167d2d
Gabriel Barreto <gbrlbarreto72@hotmail.com>
2022-11-10 19:47:28 -0300

    Acrescentando .gitignore

--- gestaoclientes2/settings.py
+++ gestaoclientes2/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'gestaoclientes2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'medicalSys',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
