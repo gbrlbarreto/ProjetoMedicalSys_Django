51fa9b185d489e7544349ef3e471abc85bc5274d
Gabriel Barreto <gbrlbarreto72@hotmail.com>
2022-11-10 20:22:27 -0300

    Config Heroku

--- gestaoclientes2/settings.py
+++ gestaoclientes2/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nb-pzw#u$hiwiz&^gsy&^!n_%#gi$ga_28$ly#f9x1z*tghwj_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['medicalsys22.herokuapp.com/'] 
+#ALLOWED_HOSTS = [, 'localhost'] 
+#Endereços que irão dar acesso ao aplicativo
 
 
 # Application definition
@@ -81,16 +85,9 @@ WSGI_APPLICATION = 'gestaoclientes2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'medicalSys',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
 # Password validation
@@ -142,7 +139,7 @@ LOGIN_URL = '/login/' #Tela de login
 
 LOGIN_REDIRECT_URL = 'home' #Nome da URL após fazer login no sistema
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [ #pasta de arquivos estáticos
     'statics',
