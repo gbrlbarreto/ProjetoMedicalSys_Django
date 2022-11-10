d5838e14d971cdc762978b728c33f5548436bb95
Gabriel Barreto <gbrlbarreto72@hotmail.com>
2022-11-10 20:50:03 -0300

    alt heroku

--- gestaoclientes2/settings.py
+++ gestaoclientes2/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['medicalsys22.herokuapp.com/'] 
+ALLOWED_HOSTS = ['medicalsys22.herokuapp.com'] 
 #ALLOWED_HOSTS = [, 'localhost'] 
 #Endereços que irão dar acesso ao aplicativo
 
