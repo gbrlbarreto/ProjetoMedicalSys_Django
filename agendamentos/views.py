d330b6c1f1bc1a172241992b87a680a37f11e789
gbrlbarreto <gbrlbarreto72@hotmail.com>
2025-08-23 01:02:57 -0300

    feat: acrescentando filtro de busca para agendamentos

--- agendamentos/views.py
+++ agendamentos/views.py
@@ -1,29 +1,61 @@
+from datetime import datetime
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Agendamento
 from .forms import AgendamentoForm
+from django.db.models import Q
 
 #@login_required
 #def agendamentos_list(request): #toda função deve retornar um request
 #    agendamentos = Agendamento.objects.all() #buscar todos os dados do banco de dados
 #    return render(request, 'agendamento.html', {'agendamentos': agendamentos})
 
+#@login_required
+#def agendamentos_list(request):
+#    agendamentos = (Agendamento.objects
+#        .filter(arquivado=False)   # apenas não arquivados
+#        .select_related('paciente')  # otimiza consulta
+#        .order_by('data', 'hora')  # mais recentes primeiro
+#    )
+#    return render(request, 'agendamento.html', {'agendamentos': agendamentos})
+
 @login_required
 def agendamentos_list(request):
-    agendamentos = (Agendamento.objects
-        .filter(arquivado=False)   # apenas não arquivados
-        .select_related('paciente')  # otimiza consulta
-        .order_by('data', 'hora')  # mais recentes primeiro
+    busca = request.GET.get('busca', '')
+    agendamentos = (
+        Agendamento.objects
+        .filter(arquivado=False)
+        .select_related('paciente')
     )
+    if busca:
+        filtros = Q(paciente__nome__icontains=busca) | Q(status__icontains=busca)
+        try:
+            data_formatada = datetime.strptime(busca, '%d/%m/%Y').date()
+            filtros |= Q(data=data_formatada)
+        except ValueError:
+            pass  # Não é uma data válida, então ignora
+        agendamentos = agendamentos.filter(filtros)
+
+    agendamentos = agendamentos.order_by('data', 'hora')
     return render(request, 'agendamento.html', {'agendamentos': agendamentos})
 
 @login_required
 def agendamentos_arquivados(request):
+    busca = request.GET.get('busca', '')
     agendamentos = (Agendamento.objects
         .filter(arquivado=True)
         .select_related('paciente')
-        .order_by('data', 'hora')
     )
+    if busca:
+        filtros = Q(paciente__nome__icontains=busca) | Q(status__icontains=busca)
+        try:
+            data_formatada = datetime.strptime(busca, '%d/%m/%Y').date()
+            filtros |= Q(data=data_formatada)
+        except ValueError:
+            pass  # Não é uma data válida, então ignora
+        agendamentos = agendamentos.filter(filtros)
+
+    agendamentos = agendamentos.order_by('data', 'hora')
     return render(request, 'agendamento_arquivado.html', {'agendamentos': agendamentos})
 
 @login_required
