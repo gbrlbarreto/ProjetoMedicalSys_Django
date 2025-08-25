289f7ef3c02c793de9e16880a2d1408280504996
gbrlbarreto <gbrlbarreto72@hotmail.com>
2025-08-24 22:09:10 -0300

    feat: personalizando forms de cliente

--- clientes/forms.py
+++ clientes/forms.py
@@ -1,4 +1,5 @@
 from dataclasses import field
+from django import forms
 from django.forms import ModelForm
 from .models import Person
 
@@ -8,3 +9,15 @@ class PersonForm(ModelForm):
         model = Person
         fields = ['nome', 'telefone', 'cep', 'rua', 'numero', 'complemento', 'bairro', 
         'cidade', 'estado', 'país', ]
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'telefone': forms.TextInput(attrs={'class': 'form-control'}),
+            'cep': forms.TextInput(attrs={'class': 'form-control'}),
+            'rua': forms.TextInput(attrs={'class': 'form-control'}),
+            'numero': forms.TextInput(attrs={'class': 'form-control'}),
+            'complemento': forms.TextInput(attrs={'class': 'form-control'}),
+            'bairro': forms.TextInput(attrs={'class': 'form-control'}),
+            'cidade': forms.TextInput(attrs={'class': 'form-control'}),
+            'estado': forms.TextInput(attrs={'class': 'form-control'}),
+            'país': forms.TextInput(attrs={'class': 'form-control'}),
+        }
